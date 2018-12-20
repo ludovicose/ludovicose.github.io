87a3ca829b00d3bc5715d50c412d2b4b394bed64
Aibek Muratbekov <aibek.muratbekov@gmail.com>
2018-12-20 22:14:25 +0600

    Display Weather

--- weather/app.js
+++ weather/app.js
@@ -1,14 +1,17 @@
 // Init Weather object
 const weather = new Weather('Almaty', 'KZ');
 
+// Init UI
+const ui = new UI();
+
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
 
 
 function getWeather() {
     weather.getWeather()
-        .then(result => {
-            console.log(result);
+        .then(results => {
+            ui.paint(results);
         })
         .catch(err => console.log(err));
 
